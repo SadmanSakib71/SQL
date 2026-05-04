341cf6ad4a040a2f6abb525cec9cf603e9e6726d
Sadman Sakib <155054940+SadmanSakib71@users.noreply.github.com>
2026-05-04 17:26:15 +0600

    Insert

---
+++ SQL DML Commands (INSERT, UPDATE, DELETE )/SQLQuery - INSERT - Manually.sql
@@ -0,0 +1,13 @@
+/* 
+INSERT INTO customers (id,first_name,country,score)
+VALUES
+	(6,'sakib','Bangladesh',71),
+	(7, 'ahona','Barisal', 52)
+*/
+
+INSERT INTO customers (id,first_name)
+VALUES
+	(8,'sakib')
+--in this situation the other two column will be null. we can't null the id column.
+--value and column should be on the same alignment
+
