e8746b36c477a2b43415ffa5e54eb04dd92e6c8c
Sadman Sakib <155054940+SadmanSakib71@users.noreply.github.com>
2026-05-04 18:05:00 +0600

    UPDATE

--- SQL DML Commands (INSERT, UPDATE, DELETE )/SQLQuery - UPDATE.sql
+++ SQL DML Commands (INSERT, UPDATE, DELETE )/SQLQuery - UPDATE.sql
@@ -1,17 +1,13 @@
-
-
-
-
-
 /*change the score of customer id 6 to 0
 
 UPDATE customers
 SET score = 0
 WHERE id = 6
+
 */
 
 /*change the score of customer id 2 to 69
-and change the country to bangladesh */
+and change the country to bangladesh 
 
 UPDATE customers 
 SET 
@@ -20,4 +16,10 @@ SET
 
 WHERE id = 2
 
+*/
 
+/* upate all the customer with null score and setting their score to 0 */
+
+UPDATE customers
+SET score = 0
+WHERE score IS NULL
