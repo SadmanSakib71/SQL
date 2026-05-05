7a822ff2d14429cc718a25d8d6d81966e3a3867a
Sadman Sakib <155054940+SadmanSakib71@users.noreply.github.com>
2026-05-05 11:29:46 +0600

    comparison

---
+++ SQL WHERE Conditions/SQLQuery- COMPARISION operators.sql
@@ -0,0 +1,40 @@
+/*get all the customers from germany
+
+SELECT * 
+FROM customers
+WHERE country = 'Germany'
+*/
+
+/*get all the customers from not germany
+
+SELECT * 
+FROM customers
+WHERE country != 'Germany'
+*/
+
+/* get all the customers score more than 500
+
+SELECT * 
+FROM customers
+WHERE score > 500
+*/
+
+/* get all the customers score more than or equal 500
+
+SELECT * 
+FROM customers
+WHERE score >= 500
+*/
+
+/* get all the customers score less than 500
+
+SELECT * 
+FROM customers
+WHERE score < 500
+*/
+
+-- get all the customers score less than or equal 500
+
+SELECT * 
+FROM customers
+WHERE score <= 500
