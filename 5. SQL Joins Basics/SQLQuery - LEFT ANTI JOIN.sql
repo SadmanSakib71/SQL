d2d2ddd5c3d8cd23361715d7f72f2813db697f9a
Sadman Sakib <155054940+SadmanSakib71@users.noreply.github.com>
2026-05-05 17:40:55 +0600

    LEFT ANTI JOIN

---
+++ 5. SQL Joins Basics/SQLQuery - LEFT ANTI JOIN.sql
@@ -0,0 +1,13 @@
+-- get all the  customers who haven't place any order
+
+SELECT 
+	c.id,
+	c.first_name,
+	c.country,
+	o.order_id,
+	o.sales
+-- customers table name as c and orders is o for shortcut.cuz sometimes column name can be same within two table.
+FROM customers AS c
+LEFT JOIN orders AS o
+ON c.id = o.customer_id
+WHERE o.customer_id IS NULL
