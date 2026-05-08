6ad70406eead0a4676dfd3bc35bd114cebba07f7
Sadman Sakib <155054940+SadmanSakib71@users.noreply.github.com>
2026-05-08 16:24:43 +0600

    date trun

--- 9. SQL Functions/SQL Date & Time Functions/SQL - DATETRUNC.sql
+++ 9. SQL Functions/SQL Date & Time Functions/SQL - DATETRUNC.sql
@@ -7,3 +7,10 @@ SELECT
 	DATETRUNC(YEAR , CreationTime) AS years_DT
 
 FROM Sales.Orders
+
+-- how many orders in a month
+SELECT 
+	DATETRUNC(MONTH , CreationTime) AS Month_DT,
+	COUNT(*)
+	FROM Sales.Orders
+GROUP BY DATETRUNC(MONTH , CreationTime)
