031851aa4989d29eb86870281d4502c632e3d292
Sadman Sakib <155054940+SadmanSakib71@users.noreply.github.com>
2026-05-10 17:54:42 +0600

    handle null

---
+++ 9. SQL Functions/SQL NULL Functions  COALESCE, ISNULL, NULLIF, IS (NOT) NULL/SQL - HANDLE NULL.sql
@@ -0,0 +1,15 @@
+-- Display the full name of customers in a single field
+-- by merging their first and last names,
+-- and add 10 bonus points to each customer's score.
+
+SELECT 
+FirstName,
+LastName,
+Score,
+COALESCE(FirstName, '') AS New_First_name,
+COALESCE(LastName, '') As New_Last_name,
+COALESCE(Score, 0) AS New_Score,
+COALESCE(FirstName, '') +  ' ' + COALESCE(LastName, '') AS Full_name,
+Score + 10 AS Bonus_point
+
+FROM Sales.Customers
