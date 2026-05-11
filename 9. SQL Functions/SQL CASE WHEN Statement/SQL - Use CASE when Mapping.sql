c09cbf63f29172edfe2f61859f93a1620b360452
Sadman Sakib <155054940+SadmanSakib71@users.noreply.github.com>
2026-05-11 08:50:07 +0600

    use of cases

--- 9. SQL Functions/SQL CASE WHEN Statement/SQL - Use CASE when Mapping.sql
+++ 9. SQL Functions/SQL CASE WHEN Statement/SQL - Use CASE when Mapping.sql
@@ -12,7 +12,12 @@ CASE
 	WHEN Country = 'Germany' THEN 'DB'
 	WHEN Country = 'USA' THEN 'US'
 	ELSE 'N/A'
-END Country_code
+END Country_code,
+-- use CASE 2
+CASE Country
+	WHEN 'Germany' THEN 'DB'
+	WHEN 'USA' THEN  'US'
+ENd Country_code_2
 FROM Sales.Customers
 
 
