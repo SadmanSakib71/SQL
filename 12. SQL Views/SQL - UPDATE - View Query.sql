5b2af5019e8beb915849a0b856b4387bee046614
Sadman Sakib <155054940+SadmanSakib71@users.noreply.github.com>
2026-05-21 23:57:59 +0600

    update view

---
+++ 12. SQL Views/SQL - UPDATE - View Query.sql
@@ -0,0 +1,12 @@
+-- Delete the view first
+DROP VIEW  Sales.V_monthly_summary
+
+-- After create /update the view
+CREATE VIEW Sales.V_monthly_summary AS (
+SELECT 
+	DATETRUNC(month,OrderDATE) AS Order_Month,
+	SUM(Sales) AS Total_sales,
+	COUNT(OrderID) AS Total_orders,
+	SUM(Quantity) AS Total_quantity
+FROM  Sales.Orders
+GROUP BY DATETRUNC(month,OrderDATE))
