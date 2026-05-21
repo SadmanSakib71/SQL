e72c42a3f5322353cbf4d511a73a07321c0e7f37
Sadman Sakib <155054940+SadmanSakib71@users.noreply.github.com>
2026-05-21 23:36:45 +0600

    create view

---
+++ 12. SQL Views/SQL - CREATE - The View Query.sql
@@ -0,0 +1,9 @@
+
+CREATE VIEW V_monthly_summary AS (
+SELECT 
+	DATETRUNC(month,OrderDATE) AS Order_Month,
+	SUM(Sales) AS Total_sales,
+	COUNT(OrderID) AS Total_orders,
+	SUM(Quantity) AS Total_quantity
+FROM  Sales.Orders
+GROUP BY DATETRUNC(month,OrderDATE))
