d8f26a7ac3cd806d67eee22445d43b287fd1060f
Sadman Sakib <155054940+SadmanSakib71@users.noreply.github.com>
2026-05-22 14:56:49 +0600

    use case data secuirity

---
+++ 12. SQL Views/SQL - Use Case Data Secqurity - Query.sql
@@ -0,0 +1,26 @@
+-- Provide a view for EU Sales Team
+-- that combines details from All tables
+-- And excludes Data related to the USA
+
+CREATE VIEW Sales.V_order_details_EU AS (
+SELECT 
+	o.OrderID,
+	o.CustomerID,
+	o.OrderDate,
+	p.Product,
+	p.Category As Product_Category,
+	COALESCE(c.FirstName, '') + ' ' + COALESCE(c.LastName,'') As Customer_Name,
+	c.Country As Customer_Country,
+	COALESCE(e.FirstName, '') + ' ' + COALESCE(e.LastName,'') As Employee_Name,
+	e.Department As Employee_Department,
+	o.Sales,
+	o.Quantity
+FROM Sales.Orders AS o
+LEFT JOIN Sales.Products AS p
+ON o.ProductID = p.ProductID
+LEFT JOIN Sales.Customers AS c
+ON o.CustomerID = c.CustomerID
+LEFT JOIN Sales.Employees AS e
+ON o.SalesPersonID = e.EmployeeID
+WHERE c.Country != 'USA'
+)
